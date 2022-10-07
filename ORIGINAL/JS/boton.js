70ce77ca566870097762f27a0eecbf0dc5180f89
CLZJUAN <103151953+CLZJUAN@users.noreply.github.com>
2022-10-07 04:18:06 -0300

    Update boton.js

--- ORIGINAL/JS/boton.js
+++ ORIGINAL/JS/boton.js
@@ -1,21 +1,46 @@
-const button = document.createElement('button'); 
-button.type = 'button'; 
-button.innerText = 'salir'; 
+let = document.getElementById('nombre');
+let = document.getElementById('apellido');
+let = document.getElementById('numero');
+let = document.getElementById('correo');
+let = document.getElementById('enviar');
+let = document.getElementById('reset');
 
-button.style.backgroundColor = "red"
-button.style.paddingTop =  "56px"
-button.style.fontSize = "66px"
-button.style.margin = "40px"
+nombre.style.color = "red";
+nombre.style.border = "36px";
+nombre.style.padding = "30px"
+nombre.style.margin = "22px";
+nombre.style.backgroundColor = "violet";
 
+apellido.style.color = "green";
+apellido.style.border = "40px";
+apellido.style.padding = "30px";
+apellido.style.backgroundColor = "orange"
+apellido.style.margin = "22px";
 
+numero.style.color = "green";
+numero.style.border = "40px";
+numero.style.padding = "30px";
+numero.style.backgroundColor = "yellow";
+numero.style.margin = "22px";
 
-    
+correo.style.color = "green";
+correo.style.border = "40px";
+correo.style.padding = "30px";
+correo.style.backgroundColor = "black";
+correo.style.margin = "22px";
 
-    
+enviar.style.border = "40px";
+enviar.style.padding = "80px";
+enviar.style.backgroundColor = "black";
+enviar.style.margin = "22px";
+enviar.style.color = "azure"
+enviar.style.fontSize = "36PX"
+enviar.style.borderRadius = "9%";
 
-
-
-
-
-
-//proximamente//
+reset.style.border = "70px";
+reset.style.padding = "80px";
+reset.style.backgroundColor = "black";
+reset.style.margin = "22px";
+reset.style.color ="azure"
+reset.style.fontSize = "36px"
+reset.style.borderRadius = "9%";
