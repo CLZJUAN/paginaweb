b4fefa07754591c7f46c21760b714ceb65f70267
CLZJUAN <103151953+CLZJUAN@users.noreply.github.com>
2022-10-07 02:51:36 -0300

    Add files via upload

---
+++ ORIGINAL/JS/boton.js
@@ -0,0 +1,21 @@
+const button = document.createElement('button'); 
+button.type = 'button'; 
+button.innerText = 'salir'; 
+
+button.style.backgroundColor = "red"
+button.style.paddingTop =  "56px"
+button.style.fontSize = "66px"
+button.style.margin = "40px"
+
+
+
+    
+
+    
+
+
+
+
+
+
+//proximamente//
