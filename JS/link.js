4a88f1e5323baebfec3163f5bc9bffcb067802e7
CLZJUAN <103151953+CLZJUAN@users.noreply.github.com>
2022-10-08 20:17:03 -0300

    Update link.js

--- JS/link.js
+++ JS/link.js
@@ -1,29 +1,10 @@
 "use stric"
-/*console.log(document.getElementsByClassName('CAJAFLEX'));*/
 
-/*const info =document.createElement("button");
-info.innerText = "info";
-info.style.backgroundColor = "orange";
-info.style.fontFamily = "italy";
-info.style.fontSize = "36px";
-info.style.borderRadius = "22px";
-info.style.borderColor = "red";
-info.style.padding = "30px";
-info.style.margin = "40px";*/
+*/ todavioa FALTA */
+let = document.getElementById('btn');
 
-
-//LO COMENTO POR QUE TENGO QUE LLAMAR  htmlcollectionof>element //
-
-// pero queria guardar los estilos que les iva a dar estan en fase prueba //
-
-/*console.log(document.getElementById('SOLAR'));
-const section = document.getElementsByClassName('CAJAFLEX');
-const info =document.createElement('button');
-
-info.innerText(sol)*/
-console.log(document.getElementsByClassName('CAJAFLEX'));
-
-
+btn.style.color = "red";
+btn.style.padding= "34px";
 
 
 
