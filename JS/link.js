426a652351f80677f314b8781fe8c7d8ee93d3bf
CLZJUAN <103151953+CLZJUAN@users.noreply.github.com>
2022-10-08 20:40:41 -0300

    Update link.js

--- JS/link.js
+++ JS/link.js
@@ -1,11 +1,14 @@
 "use stric"
 
 */ todavioa FALTA */
-let = document.getElementById('btn');
+
+let = document.getElementById("btn");
 
 btn.style.color = "red";
 btn.style.padding= "34px";
-
+btn.style.margin = "30px";
+btn.style.fontSize = "66px";
+btn.style.backgroundColor = "black";
 
 
 
