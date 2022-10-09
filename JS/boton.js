e274b5a8b8801109d54b1033b7c7ce01dcb1b9cf
CLZJUAN <103151953+CLZJUAN@users.noreply.github.com>
2022-10-08 23:35:51 -0300

    Update boton.js

--- JS/boton.js
+++ JS/boton.js
@@ -1,3 +1,4 @@
+"use stric"
 let = document.getElementById('nombre');
 let = document.getElementById('apellido');
 let = document.getElementById('numero');
@@ -5,55 +6,58 @@ let = document.getElementById('correo');
 let = document.getElementById('enviar');
 let = document.getElementById('reset');
 
+
 nombre.style.color = "red";
 nombre.style.border = "40px";
-nombre.style.padding = "30px"
-nombre.style.margin = "22px";
+nombre.style.padding = "20px";
+nombre.style.margin = "5px";
 nombre.style.backgroundColor = "violet";
 nombre.style.fontSize = "23px";
 
 apellido.style.color = "green";
-apellido.style.border = "40px";
-apellido.style.padding = "30px";
+apellido.style.border = "20px";
+apellido.style.padding = "20px";
 apellido.style.backgroundColor = "orange"
-apellido.style.margin = "22px";
+apellido.style.margin = "5px";
 apellido.style.fontSize = "23px";
 
 numero.style.color = "green";
-numero.style.border = "40px";
+numero.style.border = "20px";
 numero.style.padding = "30px";
 numero.style.backgroundColor = "yellow";
-numero.style.margin = "22px";
+numero.style.margin = "5px";
 numero.style.fontSize = "23px";
 
 correo.style.color = "green";
 correo.style.border = "40px";
-correo.style.padding = "30px";
+correo.style.padding = "20px";
 correo.style.backgroundColor = "black";
 correo.style.margin = "22px";
 correo.style.fontSize = "23px";
 
-enviar.style.border = "40px";
-enviar.style.padding = "80px";
-enviar.style.backgroundColor = "black";
-enviar.style.margin = "22px";
-enviar.style.color = "azure"
-enviar.style.fontSize = "36PX"
-enviar.style.borderRadius = "9%";
-
 reset.style.border = "70px";
-reset.style.padding = "80px";
+reset.style.padding = "30px";
 reset.style.backgroundColor = "black";
-reset.style.margin = "22px";
-reset.style.color ="azure"
-reset.style.fontSize = "36px"
+reset.style.margin = "5px";
+reset.style.color ="azure";
+reset.style.fontSize = "36px";
 reset.style.borderRadius = "9%";
 
-function validar() {
+enviar.style.border = "40px";
+enviar.style.padding = "20px";
+enviar.style.backgroundColor = "red";
+enviar.style.margin = "5px";
+enviar.style.color = "azure";
+enviar.style.fontSize = "36px";
+enviar.style.borderRadius = "9%";
+
+
+function validacion() {
 let nombre;
 let = document.getElementById('nombre').value;
 let = document.getElementById('numero').value;
 let = document.getElementById('correo').value;
+
 
 if (nombre === " ") {
     alert("no se registro ningun dato intente nuevamente")
