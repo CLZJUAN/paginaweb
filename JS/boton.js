079e6d5f032e8b98c2ab1a0635b4416e060dfe73
CLZJUAN <103151953+CLZJUAN@users.noreply.github.com>
2022-10-10 16:21:37 -0300

    Update boton.js

--- JS/boton.js
+++ JS/boton.js
@@ -8,21 +8,21 @@ let = document.getElementById("envio");
 let = document.getElementById("reset");
 let = document.getElementById("faltandatos");
 
-nombre.style.color = "red";
+nombre.style.color = "white";
 nombre.style.border = "40px";
 nombre.style.padding = "20px";
 nombre.style.margin = "5px";
 nombre.style.backgroundColor = "violet";
 nombre.style.fontSize = "23px";
 
-apellido.style.color = "green";
+apellido.style.color = "white";
 apellido.style.border = "20px";
 apellido.style.padding = "20px";
 apellido.style.backgroundColor = "orange"
 apellido.style.margin = "5px";
 apellido.style.fontSize = "23px";
 
-numero.style.color = "green";
+numero.style.color = "white";
 numero.style.border = "20px";
 numero.style.padding = "20px";
 numero.style.backgroundColor = "yellow";
@@ -52,15 +52,13 @@ reset.style.color ="azure";
 reset.style.fontSize = "36px";
 reset.style.borderRadius = "9%";
 
-faltandatos.style.color = "fuccia";
+faltandatos.style.color = "red";
 
 
 
 let forms = document.getElementById("formularioss");
 forms.addEventListener("submit" ,function (evt){
 evt.preventDefault();
-
-
 let corrupto =[];
 
 if (nombre.value === null | nombre.value === " ") {
