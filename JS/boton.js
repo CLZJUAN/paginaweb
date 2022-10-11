cb76a4ffed4528d973dc749052e6a6883c005d1e
CLZJUAN <103151953+CLZJUAN@users.noreply.github.com>
2022-10-11 02:36:07 -0300

    Update boton.js

--- JS/boton.js
+++ JS/boton.js
@@ -10,45 +10,45 @@ const reset = document.getElementById("reset");
 const form = document.getElementById("form");
 
 nombre.style.color = "white";
-nombre.style.border = "40px";
+nombre.style.border = "30px";
 nombre.style.padding = "20px";
-nombre.style.margin = "5px";
+nombre.style.margin = "20px";
 nombre.style.backgroundColor = "violet";
 nombre.style.fontSize = "23px";
 
 apellido.style.color = "white";
-apellido.style.border = "20px";
+apellido.style.border = "30px";
 apellido.style.padding = "20px";
 apellido.style.backgroundColor = "orange"
-apellido.style.margin = "5px";
+apellido.style.margin = "20px";
 apellido.style.fontSize = "23px";
 
 numero.style.color = "white";
-numero.style.border = "20px";
+numero.style.border = "30px";
 numero.style.padding = "20px";
 numero.style.backgroundColor = "yellow";
-numero.style.margin = "5px";
+numero.style.margin = "20px";
 numero.style.fontSize = "23px";
 
 correo.style.color = "white";
 correo.style.border = "30px";
 correo.style.padding = "20px";
 correo.style.backgroundColor = "black";
-correo.style.margin = "5px";
+correo.style.margin = "20px";
 correo.style.fontSize = "23px";
 
-envio.style.border = "40px";
+envio.style.border = "30px";
 envio.style.padding = "20px";
 envio.style.backgroundColor = "black";
-envio.style.margin = "22px";
+envio.style.margin = "20px";
 envio.style.color = "azure"
 envio.style.fontSize = "36px"
 envio.style.borderRadius = "9%";
 
-reset.style.border = "40px";
+reset.style.border = "30px";
 reset.style.padding = "20px";
 reset.style.backgroundColor = "black";
-reset.style.margin = "22px";
+reset.style.margin = "20px";
 reset.style.color = "azure"
 reset.style.fontSize = "36px"
 reset.style.borderRadius = "9%";
@@ -57,10 +57,8 @@ msj.style.color = "white";
 msj.style.border = "30px";
 msj.style.padding = "70px";
 msj.style.backgroundColor = "red";
-msj.style.margin = "5px";
+msj.style.margin = "20px";
 msj.style.fontSize = "23px";
-
-
 
 
 
@@ -99,4 +97,3 @@ form.addEventListener("submit", e=>{
 
 
 })
-
